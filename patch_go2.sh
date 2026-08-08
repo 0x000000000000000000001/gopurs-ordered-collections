bdde4711499797d9ec5e36585e2dbb75d3befb8b
0x1 <kevin.francart3@gmail.com>
2026-08-08 19:50:23 +0200

    misc

--- patch_go2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git checkout src/Data/Map/Internal.go
-sed -i.bak 's/return f(acc)(key)(value)/println("FoldlImpl value type:"); return f(acc)(key)(value)/' src/Data/Map/Internal.go
--- patch_unbox.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i.bak 's/v := \*(\*any)(ptr)/if uintptr(ptr) < 4096 { panic(fmt.Sprintf("Invalid ptr in Unbox[int]: %v", ptr)) }\n\t\t\t\tv := \*(\*any)(ptr)/g' output/gopurs_runtime/runtime.go
